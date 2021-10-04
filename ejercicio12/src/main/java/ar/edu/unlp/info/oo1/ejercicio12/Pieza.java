6eae99a3e1bfb69713df75d3ba78b1ecaa5743e5
Gregorio Maclen <gregorio.maclen@gmail.com>
2021-10-04 20:34:42 -0300

    .

---
+++ ejercicio12/src/main/java/ar/edu/unlp/info/oo1/ejercicio12/Pieza.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.info.oo1.ejercicio12;
+
+public abstract class Pieza {
+	private String material;
+	private String color;
+	
+}
---
+++ ejercicio12/src/main/java/ar/edu/unlp/info/oo1/ejercicio12/ReporteDeConstruccion.java
@@ -0,0 +1,16 @@
+package ar.edu.unlp.info.oo1.ejercicio12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReporteDeConstruccion {
+	private List<Pieza> piezas = new ArrayList<>();
+	
+	public double volumenDeMaterial(String material) {
+		return 0;
+	}
+	
+	public double superficieDeColor(String color) {
+		return 0;
+	}
+}
